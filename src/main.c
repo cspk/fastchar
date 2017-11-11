aed76f4d4d0324f509718b5d41de11c8978e3861
Pavel Serebryakov <cspk@list.ru>
2017-11-11 19:40:20 +0500

    Add stub GTK application code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,10 @@
-int main(void) {
-	return 0;
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+	GtkApplication *app = gtk_application_new("org.gtk."PROJECT_NAME,
+		G_APPLICATION_FLAGS_NONE);
+	int status = g_application_run(G_APPLICATION(app), argc, argv);
+
+	g_object_unref(app);
+	return status;
 }
