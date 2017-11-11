956b9419e36d42a5709358bcac709e4ea35d3c8d
Pavel Serebryakov <cspk@list.ru>
2017-11-11 21:51:59 +0500

    Add confing file path resolution

---
+++ src/cfg.h
@@ -0,0 +1,6 @@
+#ifndef CFG_H
+#define CFG_H
+
+#define CFG_FILENAME "." PROJECT_NAME
+
+#endif /* CFG_H */
--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <gtk/gtk.h>
 
+#include "cfg.h"
 #include "ui.h"
 
 int main(int argc, char *argv[]) {
+	const char *user_home = getenv("HOME");
+	if (!user_home) {
+		fprintf(stderr, "Could not get user HOME!\n");
+		return 1;
+	}
+	gchar *cfg_path = g_strjoin("/", user_home, CFG_FILENAME, NULL);
+
 	GtkApplication *app = gtk_application_new("org.gtk."PROJECT_NAME,
 		G_APPLICATION_FLAGS_NONE);
 	g_signal_connect(app, "activate", G_CALLBACK(ui_init), NULL);
