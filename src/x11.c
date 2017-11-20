e54ab9596c08726944c0e1c8415333bb7626b2bc
Pavel Serebryakov <cspk@list.ru>
2017-11-20 22:21:48 +0500

    x11: improve error handling on char typing

--- src/x11.c
+++ src/x11.c
@@ -1,10 +1,15 @@
+#include <assert.h>
 #include <X11/Xlib.h>
 #include <fakekey/fakekey.h>
+#include "dbg.h"
 
 void x11_type_char(const char *utf8_char) {
     Display *disp = XOpenDisplay(NULL);
+    assert(disp);
 
     FakeKey *fk = fakekey_init(disp);
+    assert(fk);
+
     fakekey_press(fk, (const unsigned char*)utf8_char, -1, 0);
     fakekey_release(fk);
 
