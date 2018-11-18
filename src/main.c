5b10ff25e58a4e90a87698417cf0f19e8c470de7
Pavel Serebryakov <cspk@list.ru>
2018-11-18 22:07:35 +0500

    options: add 'version'

--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@
 #include "cfg.h"
 #include "ui.h"
 
+static gboolean arg_version;
+
+static GOptionEntry params[] =
+{
+  { "version", 0, 0, G_OPTION_ARG_NONE, &arg_version, "Show version", NULL },
+  { NULL }
+};
+
 static int g_main(void);
 
 int main(int argc, char *argv[]) {
@@ -12,6 +20,22 @@ int main(int argc, char *argv[]) {
 	GApplicationFlags app_flags = G_APPLICATION_FLAGS_NONE;
 	GtkApplication *app = gtk_application_new(app_name, app_flags);
 
+	GOptionContext *context = g_option_context_new("- type in arbitrary symbols");
+	g_option_context_add_main_entries(context, params, NULL);
+	g_option_context_add_group(context, gtk_get_option_group(TRUE));
+
+	GError *error;
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
+		fprintf(stderr, "error parsing program arguments\n");
+		return 1;
+	}
+
+	if (arg_version) {
+		printf("v%s\n", PROJECT_VERSION);
+		g_object_unref(app);
+		return 0;
+	}
+	
 	g_signal_connect(app, "activate", G_CALLBACK(g_main), NULL);
 	int status = g_application_run(G_APPLICATION(app), argc, argv);
 
